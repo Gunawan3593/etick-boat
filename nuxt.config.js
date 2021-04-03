6c8eb48cd9a165086d2f76cf54a0047e7c5d59b9
Adi Gunawan <broken.wayan@gmail.com>
2021-04-04 06:58:32 +0800

    uhhh

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,12 +65,6 @@ export default {
     clientConfigs: {
       default: {
         httpEndpoint: (process.env.NODE_ENV === 'production') ? 'https://etick-boat-server.herokuapp.com/graphql' : 'http://localhost:4000/graphql',
-        httpLinkOptions: {
-          fetchOptions: {
-            mode: 'cors' //Cors Needed for external Cross origins, need to allow headers from server
-          },
-          credentials: "omit" //must be omit to support application/json content type 
-        }
       }
     },
     /**
